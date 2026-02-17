bb1fe07d957522dd2ef6d2dcf4d74cb0abc88269
Johannes Niedens <johannesniedens@gmail.com>
2026-02-17 01:37:19 -0300

    merged leafnode and parentnode into the htmlnode file

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -18,3 +18,37 @@ class HTMLNode:
     
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag=tag, value=value, props=props)
+    
+    def to_html(self):
+        if not self.value:
+            raise ValueError("invalid HTML: no value")
+        if not self.tag:
+            return self.value
+        else:
+            res = (f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>")
+            return res
+
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, {self.value}, {self.props})"
+
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag=None, children=None, props=None):
+        super().__init__(tag, None, children, props)
+    
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("invalid HTML: no tag")
+        if self.children is None:
+            raise ValueError("invalid HTML: children missing")
+        html = f"<{self.tag}{self.props_to_html()}>"
+        for child in self.children:
+            html += child.to_html()
+        html += f"</{self.tag}>"
+        return html
+    
